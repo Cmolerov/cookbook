7a200444a7564de6753759c4d1e13d66d997a87a
carlos molero <cmolero130@gmail.com>
2021-01-06 12:19:21 -0500

    welcome page css

--- react-app/src/components/WelcomePage.js
+++ react-app/src/components/WelcomePage.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function WelcomePage() {
     return (
-        <div>
-            <div className="welcome_main-div">
-
-            </div>
+        <div className="welcome_main-div">
+            <div></div>
         </div>
-    )
+    );
 }

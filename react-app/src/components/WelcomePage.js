b3601b3fe5e77e57d3b8a8fdffb5bac2ab5dbbab
carlos molero <cmolero130@gmail.com>
2021-01-06 15:40:06 -0500

    add container and quote

--- react-app/src/components/WelcomePage.js
+++ react-app/src/components/WelcomePage.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function WelcomePage() {
     return (
         <div className="welcome_main-div">
-            <div></div>
+            <div className="welcome_quote-container">
+                <p className="quote_text">
+                    "Making a 3 star plate has never been easier"
+                    <p>-CM</p>
+                </p>
+            </div>
         </div>
     );
 }

124895148626284b4b8a6b0f4a89f7dfddc15919
carlos molero <cmolero130@gmail.com>
2021-01-06 19:26:19 -0500

    set up styling and background img

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -41,52 +41,61 @@ const SignUpForm = ({ authenticated, setAuthenticated }) => {
     return (
         <div className="signup_main-container">
             <div className="signup_left-container"></div>
-            <div className="signup_right-container"></div>
+            <div className="signup_right-container">
+                <div className="signup_form-container">
+                    <form onSubmit={onSignUp}>
+                        <div className="signup_form-wrapper">
+                            <h1 className="form_title">Umami</h1>
+                            <label className="signup_label">User Name</label>
+                            <input
+                                className="signup_input"
+                                type="text"
+                                name="username"
+                                onChange={updateUsername}
+                                value={username}
+                            ></input>
+                        </div>
+                        <div className="signup_form-wrapper">
+                            <label className="signup_label">Email</label>
+                            <input
+                                className="signup_input"
+                                type="text"
+                                name="email"
+                                onChange={updateEmail}
+                                value={email}
+                            ></input>
+                        </div>
+                        <div className="signup_form-wrapper">
+                            <label className="signup_label">Password</label>
+                            <input
+                                className="signup_input"
+                                type="password"
+                                name="password"
+                                onChange={updatePassword}
+                                value={password}
+                            ></input>
+                        </div>
+                        <div className="signup_form-wrapper">
+                            <label className="signup_label">
+                                Repeat Password
+                            </label>
+                            <input
+                                className="signup_input"
+                                type="password"
+                                name="repeat_password"
+                                onChange={updateRepeatPassword}
+                                value={repeatPassword}
+                                required={true}
+                            ></input>
+                        </div>
+                        <button className="btn-grad" type="submit">
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default SignUpForm;
-
-{
-    /* <form onSubmit={onSignUp}>
-    <div>
-        <label>User Name</label>
-        <input
-            type="text"
-            name="username"
-            onChange={updateUsername}
-            value={username}
-        ></input>
-    </div>
-    <div>
-        <label>Email</label>
-        <input
-            type="text"
-            name="email"
-            onChange={updateEmail}
-            value={email}
-        ></input>
-    </div>
-    <div>
-        <label>Password</label>
-        <input
-            type="password"
-            name="password"
-            onChange={updatePassword}
-            value={password}
-        ></input>
-    </div>
-    <div>
-        <label>Repeat Password</label>
-        <input
-            type="password"
-            name="repeat_password"
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-        ></input>
-    </div>
-    <button type="submit">Sign Up</button>
-</form>; */
-}

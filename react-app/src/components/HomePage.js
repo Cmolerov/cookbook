ea65b94d506c11aef8c887647f29486745057dd6
carlos molero <cmolero130@gmail.com>
2021-01-10 02:46:05 -0500

    add homepage component and setup frame

---
+++ react-app/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function HomePage() {
+    return (
+        <Container className="homePage_wrapper" fluid>
+            <Row>
+                <Col className="homePage_sidebar col-2"></Col>
+                <Col className="homePage_content-container">
+                    <div className="homePage_recipes-container"></div>
+                </Col>
+            </Row>
+            <p>footer</p>
+        </Container>
+    )
+}

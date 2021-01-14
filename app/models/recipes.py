286a816bd72618ae4258b2026b86a314a339589a
carlos molero <cmolero130@gmail.com>
2021-01-14 15:55:00 -0500

    migrations for recipes and tags

--- app/models/recipes.py
+++ app/models/recipes.py
@@ -1,21 +1,24 @@
 from .db import db
 from .user import User
+from .tags import Tags
+
 
 class Recipes(db.Model):
-    __tablename__='recipes'
+    __tablename__ = 'recipes'
 
-    id = db.Column(db.Integer, primary_key= True)
-    userId = db.Column(db.Integer, db.ForeignKey("users.id") nullable = False, unique = True)
-    title = db.Column(db.String(100), nullable = False)
-    description = db.Column(db.String(250), nullable = False)
-    cookTime = db.Column(db.Integer, nullable = False)
-    image = db.Column(db.String(5000), nullable = True)
-    tags = db.Column(db, db.ForeignKey("tags.id") nullable = True, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey(
+        "users.id"), nullable=False, unique=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    cookTime = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String(5000), nullable=True)
+    tags = db.Column(db.ForeignKey("tags.id"), nullable=True)
 
-   user = relationship("User", back_populates="recipes")
-   tags = relationship("Tags", back_populates="recipes")
+    user = db.relationship("User", back_populates="recipes",)
+    tags = db.relationship("Tags", back_populates="recipes",)
 
-   def __init__(self, userId, title, description, cookTime, image, tags ):
+    def __init__(self, userId, title, description, cookTime, image, tags):
         self.userId = userId
         self.title = title
         self.description = description
@@ -25,11 +28,11 @@ class Recipes(db.Model):
 
     def to_dict(self):
         return {
-        "id": self.id,
-        "userId": self.userId,
-        "title" : self.title,
-        "description" : self.description,
-        "cookTime" : self.cookTime,
-        "image" : self.image,
-        "tags" : self.tags
+            "id": self.id,
+            "userId": self.userId,
+            "title": self.title,
+            "description": self.description,
+            "cookTime": self.cookTime,
+            "image": self.image,
+            "tags": self.tagsId
         }
--- app/models/tags.py
+++ app/models/tags.py
@@ -1,21 +1,23 @@
 from .db import db
 
-class Tags(db.model):
-     __tablename__='tags'
 
-    
-    id = db.Column(db.Integer, primary_key = True)
-    tag = db.Column(db.String(50), nullable = True)
+class Tags(db.Model):
+    __tablename__ = 'tags'
 
-    recipes = relationship("Recipes", back_populates="recipes")
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(50), nullable=True)
+    recipeId = db.Column(db.Integer, db.ForeignKey(
+        "recipes.id"), nullable=False, unique=True)
 
-    def __init__(self, tag):
-        self.userId = userId
-        self.tag = tag
+    recipes = db.relationship("Recipes", back_populates="recipes",)
 
-    def to_dict(self):
+    def __init__(self, tag, recipeId):
+        self.tag = tag,
+        self.recipeId = recipeId
+
+    def to_dict(self, tag, recipeId):
         return {
-        "id": self.id,
-        "userId": self.userId,
-        "tag": self.tag
+            "id": self.id,
+            "tag": self.tag,
+            "recipeId": self.recipeId
         }

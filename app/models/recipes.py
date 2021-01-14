0ec98d30706af7970346349e060b7af7c64552b9
carlos molero <cmolero130@gmail.com>
2021-01-14 13:09:36 -0500

    starting recipe tables

---
+++ app/models/recipes.py
@@ -0,0 +1,13 @@
+from .db import db
+from .user import User
+
+class Recipes(db.Model):
+    __tablename__='recipes'
+
+    id = db.Column(db.Integer, primary_key= True)
+    userId = db.Column(db.Integer, db.ForeignKey("users.id") nullable = False, unique = True)
+    title = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(250), nullable = False)
+    cookTime = db.Column(db.Integer, nullable = False)
+    image = db.Column(db.String(5000), nullable = True)
+    tags

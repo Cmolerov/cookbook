eeff7261c490e32ad46db51276b8ecf5f0181733
carlos molero <cmolero130@gmail.com>
2021-01-15 18:37:43 -0500

    create form and add background img

--- react-app/src/components/CreateRecipe.js
+++ react-app/src/components/CreateRecipe.js
@@ -7,6 +7,38 @@ export default function CreateRecipe() {
             <Row className="recipe_form-container">
                 <Col className="create_recipes-container">
                     <h1 className="recipe_title">Create a Recipe</h1>
+                    <form className="recipe_form">
+                        <div className="recipe_input-wrapper">
+                            <label>Title</label>
+                            <input></input>
+                        </div>
+                        <div className="recipe_input-wrapper">
+                            <label>Description</label>
+                            <input></input>
+                        </div>
+                        <div className="recipe_input-wrapper">
+                            <label>Cook Time</label>
+                            <input></input>
+                        </div>
+                        <div className="recipe_input-wrapper">
+                            <label>Image</label>
+                            <input></input>
+                        </div>
+                        <div className="recipe_input-wrapper">
+                            <p>ingredients</p>
+                            <label>Measurement</label>
+                            <input></input>
+                            <label>Product</label>
+                            <input></input>
+                        </div>
+                        <div className="recipe_input-wrapper">
+                            <p>instructions</p>
+                            <label>list order</label>
+                            <input></input>
+                            <label>Ingredient</label>
+                            <input></input>
+                        </div>
+                    </form>
                 </Col>
             </Row>
         </Container>

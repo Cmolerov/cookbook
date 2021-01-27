06b999dfcb012da1c96ee2abd57c6ffe7fb72b2d
carlos molero <cmolero130@gmail.com>
2021-01-26 22:01:25 -0500

    create instruction form

---
+++ app/forms/instruction_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField
+from wtforms.validators import DataRequired
+
+
+class InstructionForm(FlaskForm):
+    list_order = IntegerField('list_order', validators=[DataRequired()])
+    instruction_text = StringField(
+        'instruction_text', validators=[DataRequired()])

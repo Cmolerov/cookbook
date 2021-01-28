eac261fef01a7c4c3e5c1b41b613396d45c8a935
carlos molero <cmolero130@gmail.com>
2021-01-28 18:46:09 -0500

    add new recipe completed

--- react-app/src/components/recipeForms.js/InstructionForm.js
+++ react-app/src/components/recipeForms.js/InstructionForm.js
@@ -1,16 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
+import { createInstruction } from "../../services/recipes";
+import IngredientsForm from "./IngredientsForm";
 
-export default function InstructionForm() {
+export default function InstructionForm({ recipe }) {
+    const [listOrder, setListOrder] = useState(1);
+    const [instruction, setInstruction] = useState("");
+    const [prev, setPrev] = useState([]);
+    const [done, setDone] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setDone(true);
+    };
+
+    const onKeyDown = async (e) => {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            let saved = await createInstruction({
+                recipeId: recipe.id,
+                list_order: listOrder,
+                instruction_text: instruction,
+            });
+            if (saved) {
+                setPrev([...prev, { listOrder, instruction }]);
+                setListOrder(listOrder + 1);
+                setInstruction("");
+            }
+        }
+    };
     return (
-        <div className="about_recipe-form-container">
-            <form>
-                <Col className="recipe_input-wrapper">
-                    <label className="recipe_form-label">Instructions</label>
-                    <input className="recipe_form-input instructions-input"></input>
-                </Col>
-            </form>
-            <button className="recipe_form-Continue-btn">Submit</button>
-        </div>
+        <>
+            {!done ? (
+                <div className="about_recipe-form-container">
+                    <form onKeyDown={onKeyDown} onSubmit={handleSubmit}>
+                        <Col
+                            style={{
+                                marginTop: "20px",
+                                marginLeft: "0",
+                                paddingLeft: "0",
+                            }}
+                            className="recipe_input-wrapper"
+                        >
+                            <label className="recipe_form-label">
+                                Instructions
+                            </label>
+                            <div
+                                style={{
+                                    marginTop: "10px",
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                }}
+                            >
+                                <label>{listOrder}.</label>
+                                <input
+                                    value={instruction}
+                                    onChange={(e) =>
+                                        setInstruction(e.target.value)
+                                    }
+                                    style={{ width: "95%" }}
+                                    className="recipe_form-input instructions-input"
+                                ></input>
+                            </div>
+                        </Col>
+                        {prev.length == 0 ? (
+                            <button
+                                disabled="true"
+                                className="recipe_form-Continue-btn"
+                            >
+                                Next
+                            </button>
+                        ) : (
+                            <button
+                                type="submit"
+                                className="recipe_form-Continue-btn"
+                            >
+                                Next
+                            </button>
+                        )}
+                    </form>
+                    <div>
+                        {prev &&
+                            prev.map(({ listOrder, instruction }) => (
+                                <>
+                                    <div
+                                        style={{
+                                            marginTop: "10px",
+                                            display: "flex",
+                                        }}
+                                    >
+                                        <label
+                                            style={{
+                                                marginRight: "20px",
+                                            }}
+                                        >
+                                            {listOrder}.
+                                        </label>
+                                        <span>{instruction}</span>
+                                    </div>
+                                </>
+                            ))}
+                    </div>
+                </div>
+            ) : (
+                <IngredientsForm recipe={recipe}></IngredientsForm>
+            )}
+        </>
     );
 }
--- react-app/src/services/recipes.js
+++ react-app/src/services/recipes.js
@@ -9,3 +9,41 @@ export const createRecipe = async (formData) => {
 
     return await response.json();
 };
+
+export const createInstruction = async (formData) => {
+    try {
+        const response = await fetch("/api/instructions/", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(formData),
+        });
+
+        if (!response.ok) {
+            throw response;
+        }
+        return await response.json();
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const createIngredient = async (formData) => {
+    try {
+        const response = await fetch("/api/ingredients/", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(formData),
+        });
+
+        if (!response.ok) {
+            throw response;
+        }
+        return await response.json();
+    } catch (e) {
+        console.log(e);
+    }
+};

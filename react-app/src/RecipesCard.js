8665af3c69d35eeaa9d67f0dd9401ed8270cd004
carlos molero <cmolero130@gmail.com>
2021-01-28 21:13:30 -0500

    added img src and tag

--- react-app/src/RecipesCard.js
+++ react-app/src/RecipesCard.js
@@ -5,9 +5,12 @@ export default function RecipesCard({ recipe }) {
     return (
         <div className="recipe-card-container">
             <div className="recipe-card">
-                <label>{recipe.title}</label>
-                <label>{recipe.description}</label>
-                <label>{recipe.cookTime}</label>
+                <img className="recipe-card_img" src={recipe.image}></img>
+                <label className="recipe-card_title">{recipe.title}</label>
+                <label className="recipe-card_description">
+                    {recipe.description}
+                </label>
+                <label className="recipe-card_time">{recipe.cookTime} min</label>
             </div>
         </div>
     );

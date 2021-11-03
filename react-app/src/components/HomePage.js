02ebdb4bffb7bebbb8e7c7a845d1f6057e1bc004
carlos molero <cmolero130@gmail.com>
2021-11-03 17:41:31 -0400

    removed sidebar

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -17,63 +17,6 @@ export default function HomePage() {
     return (
         <Container className="homePage_wrapper" fluid>
             <Row>
-                <Col className="homePage_sidebar col-1">
-                    <div className="timeOfDay">
-                        <h2>Meal Time</h2>
-                        {/* <label>
-                            breakfast
-                            <input type="checkbox"></input>
-                        </label>
-
-                        <label>
-                            Lunch
-                            <input type="checkbox"></input>
-                        </label>
-
-                        <label>
-                            Dinner
-                            <input type="checkbox"></input>
-                        </label> */}
-                    </div>
-                    <div className="timeOfDay">
-                        <h2>Diet</h2>
-                        {/* <label>
-                            Chicken
-                            <input type="checkbox"></input>
-                        </label>
-
-                        <label>
-                            Red Meat
-                            <input type="checkbox"></input>
-                        </label>
-
-                        <label>
-                            Fish
-                            <input type="checkbox"></input>
-                        </label>
-                        <label>
-                            Vegan
-                            <input type="checkbox"></input>
-                        </label> */}
-                    </div>
-                    <div className="timeOfDay">
-                        <h2>Allergies</h2>
-                        {/* <label>
-                            Seafood
-                            <input type="checkbox"></input>
-                        </label>
-
-                        <label>
-                            Gluten
-                            <input type="checkbox"></input>
-                        </label>
-
-                        <label>
-                            Dairy
-                            <input type="checkbox"></input>
-                        </label> */}
-                    </div>
-                </Col>
                 <Col className="homePage_content-container">
                     <div className="homePage_recipes-container" fluid>
                         {recipes &&

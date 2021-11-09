4c13d89e5340770d125df922a375e4ab48de267a
carlos molero <cmolero130@gmail.com>
2021-11-08 20:46:05 -0500

    footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -3,8 +3,22 @@ import { Container, Row, Col } from "react-bootstrap";
 
 export default function Footer() {
     return (
-        <Row className="footer row-12">
-            <div className="github_icon col-3"></div>
-        </Row>
+        <div className="footer-div">
+            <a
+                href="https://github.com/Cmolerov/cookbook"
+                className="fab fa-github footer-icon fa-2x footer-link"
+                target="_blank"
+            ></a>
+            <a
+                href="https://www.linkedin.com/in/carlos-molero/"
+                className="fab fa-linkedin footer-icon fa-2x footer-link"
+                target="_blank"
+            ></a>
+            <a
+                href="https://www.cmolero.com"
+                className="fas fa-portrait footer-icon fa-2x footer-link"
+                target="_blank"
+            ></a>
+        </div>
     );
 }
